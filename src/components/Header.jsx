38112280b26056c25f7f4f97f6bc61e5e0c33ad3
EntornosDAM1 <hgomezv01@educantabria.es>
2022-01-13 10:48:00 +0100

    2 files modified, 1 added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
+import {MenuItems} from '../data/MenuItems.js';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
+  }
+  changeClicked(){
+    this.setState(clicked = !this.state.clicked);
   }
 
   render() {
@@ -13,18 +17,20 @@ class Header extends React.Component {
         <div className="navbar-logo">
           <img className="logo-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/2560px-Playstation_logo_colour.svg.png" width="50" height="70"/>  
         </div>
+        
         <h1>Mi aplicación</h1>
+        
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/listas">Listas de tareas</Link>
-          </li>
-          <li>
-            <Link to="/perfil">Perfil</Link>
-          </li>
+          {/* Forma 3: map (función flecha) */}
+          {MenuItems.map((item) => {
+            return (
+              <li key={item.id} classNmae="navbar-link">
+                <Link to={item.path}>{item.title}</Link>
+              </li>
+            );
+          })}
         </ul>
+
       </nav>
     );
   }
